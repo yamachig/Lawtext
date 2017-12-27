8bc3691701348cae38bf79648b35c0964514fe11
yamachi <yamachig@users.noreply.github.com>
2017-12-27 22:53:05 +0900

    setup.pyを修正

--- setup.py
+++ setup.py
@@ -3,14 +3,17 @@ from pathlib import Path
 
 lawtext_dir = Path(__file__).parent / 'lawtext'
 
-setup(name='Lawtext',
-      version='0.1',
-      description='Plain text format for Japanese laws',
-      author='yamachi',
-      packages=['lawtext'],
-      package_data={'lawtext': [
-            str(p.relative_to(lawtext_dir))
-            for p in lawtext_dir.glob('**/*')
-      ]},
-      install_requires=['Jinja2', 'transcrypt'],
+setup(
+    name='lawtext',
+    version='0.0.1',
+    description='Plain text format for Japanese law',
+    url='https://github.com/yamachig/lawtext',
+    author='yamachi',
+    license='MIT',
+    packages=['lawtext'],
+    package_data={'lawtext': [
+        str(p.relative_to(lawtext_dir))
+        for p in lawtext_dir.glob('**/*')
+    ]},
+    install_requires=['Jinja2', 'transcrypt'],
 )
