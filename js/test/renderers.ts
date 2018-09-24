acb3f88a133f1843ecbb2002dabc4b41e1ed223a
yamachi <yamachig@users.noreply.github.com>
2018-09-24 13:52:44 +0900

    テーブルの出力を修正

--- js/test/renderers.ts
+++ js/test/renderers.ts
@@ -1,10 +1,10 @@
-import { describe, it, before } from "mocha"
+import { it, before } from "mocha"
 import * as chai from "chai"
 import { render as renderLawtext } from "../src/renderers/lawtext"
 import { parse, analyze } from "../src/parser_wrapper"
 import * as util from "../src/util"
-import { prepare, getLawList, ensureList, getLawXml } from "./prepare_test";
-import { lawDiff, LawDiffResult, LawDiffType, toTableText, ProblemStatus, withEllipsis, LawDiffElementChange, collapseChange, collapseNoChange, DiffStatus, LawDiffElementMismatch, LawDiffNoDiff, TagType } from "./util";
+import { prepare, ensureList, getLawXml } from "./prepare_test";
+import { lawDiff, LawDiffResult, LawDiffType, toTableText, ProblemStatus, withEllipsis, LawDiffElementChange, widthWOColor, DiffStatus, LawDiffElementMismatch, LawDiffNoDiff, TagType, TERMC } from "./util";
 import * as os from "os";
 import * as fs from "fs";
 import * as fsExtra from "fs-extra";
@@ -16,66 +16,45 @@ before(() => {
     ensureList();
 });
 
+const LIMIT_WIDTH = 30;
+
 const tempDir = path.join(os.tmpdir(), `lawtext_core_test`);
 
-function* wrap(row: string[], width = 20): IterableIterator<string[]> {
-    let pos = 0;
-    while (row.some(s => pos < s.length)) {
-        yield row.map(s => s.slice(pos, pos + width));
-        pos += width;
-    }
-}
-
 function makeElementMismatchTable(ditem: LawDiffElementMismatch<string>, d: LawDiffResult<string>, tempOrig: string) {
-    const table: [string, string, string, string][] = [];
+    const table: string[][] = [];
     for (const { oldItem, newItem, status } of ditem.diffTable) {
         if (status === DiffStatus.NoChange) {
             if (!oldItem || !newItem) throw new Error("never");
             table.push([
                 0 <= oldItem.index ? `  ${oldItem.index.toString()}` : "",
-                withEllipsis(`${oldItem.value}`, 20),
+                withEllipsis(`${oldItem.value}`, LIMIT_WIDTH),
                 0 <= newItem.index ? `  ${newItem.index.toString()}` : "",
-                withEllipsis(`${newItem.value}`, 20),
+                withEllipsis(`${newItem.value}`, LIMIT_WIDTH),
             ]);
         } else if (status === DiffStatus.Change) {
-            Array.from(wrap([
-                oldItem && `${oldItem.value}` || "",
-                newItem && `${newItem.value}` || "",
-            ])).forEach(([oldStr, newStr], i) => {
-                table.push([
-                    !i && oldItem && `\x1b[33m* ${oldItem.index.toString()}\x1b[39m` || "",
-                    `\x1b[33m${oldStr}\x1b[39m`,
-                    !i && newItem && `\x1b[33m* ${newItem.index.toString()}\x1b[39m` || "",
-                    `\x1b[33m${newStr}\x1b[39m`,
-                ]);
-            });
+            if (!oldItem || !newItem) throw new Error("never");
+            table.push([
+                `${TERMC.YELLOW}* ${oldItem.index.toString()}${TERMC.DEFAULT}`,
+                `${TERMC.YELLOW}${oldItem.value}${TERMC.DEFAULT}`,
+                `${TERMC.YELLOW}* ${newItem.index.toString()}${TERMC.DEFAULT}`,
+                `${TERMC.YELLOW}${newItem.value}${TERMC.DEFAULT}`,
+            ]);
         } else if (status === DiffStatus.Add) {
             if (!newItem) throw new Error("never");
-            Array.from(wrap([
-                `${newItem.value}`,
-            ])).forEach(([newStr], i) => {
-                table.push([
-                    "",
-                    "",
-                    !i ? `\x1b[32m+ ${newItem.index.toString()}\x1b[39m` : "",
-                    `\x1b[32m${newStr}\x1b[39m`,
-                ]);
-            });
+            table.push([
+                "",
+                "",
+                `${TERMC.GREEN}+ ${newItem.index.toString()}${TERMC.DEFAULT}`,
+                `${TERMC.GREEN}${newItem.value}${TERMC.DEFAULT}`,
+            ]);
         } else if (status === DiffStatus.Remove) {
-            if (!oldItem) {
-                console.log(oldItem, newItem);
-                throw new Error("never");
-            }
-            Array.from(wrap([
-                `${oldItem.value}`,
-            ])).forEach(([oldStr], i) => {
-                table.push([
-                    !i ? `\x1b[31m- ${oldItem.index.toString()}\x1b[39m` : "",
-                    `\x1b[31m${oldStr}\x1b[39m`,
-                    "",
-                    "",
-                ]);
-            });
+            if (!oldItem) throw new Error("never");
+            table.push([
+                `${TERMC.RED}- ${oldItem.index.toString()}${TERMC.DEFAULT}`,
+                `${TERMC.RED}${oldItem.value}${TERMC.DEFAULT}`,
+                "",
+                "",
+            ]);
         } else { util.assertNever(status); }
     }
 
@@ -83,17 +62,31 @@ function makeElementMismatchTable(ditem: LawDiffElementMismatch<string>, d: LawD
 }
 
 function makeElementChangeTable(ditem: LawDiffElementChange<string>, d: LawDiffResult<string>, tempOrig: string) {
-    let errMsg = "";
-
-    const table: [string, string, string, string][] = [];
+    const table: string[][] = [];
     const [[oldEL, /**/], [newEL, /**/]] = [d.oldELs[ditem.oldIndex], d.newELs[ditem.newIndex]];
 
-    table.push([
-        `\x1b[33m* ${ditem.oldIndex}\x1b[39m`,
-        `<${oldEL.tag}`,
-        `\x1b[33m* ${ditem.newIndex}\x1b[39m`,
-        `<${newEL.tag}`,
-    ]);
+    if (ditem.mostSeriousStatus === ProblemStatus.Error) {
+        table.push([
+            `${TERMC.YELLOW}* ${ditem.oldIndex}${TERMC.DEFAULT}`,
+            `<${oldEL.tag}`,
+            `${TERMC.YELLOW}* ${ditem.newIndex}${TERMC.DEFAULT}`,
+            `<${newEL.tag}`,
+        ]);
+    } else if (ditem.mostSeriousStatus === ProblemStatus.Warning) {
+        table.push([
+            `${TERMC.CYAN}* ${ditem.oldIndex}${TERMC.DEFAULT}`,
+            `<${oldEL.tag}`,
+            `${TERMC.CYAN}* ${ditem.newIndex}${TERMC.DEFAULT}`,
+            `<${newEL.tag}`,
+        ]);
+    } else if (ditem.mostSeriousStatus === ProblemStatus.NoProblem) {
+        table.push([
+            `${TERMC.BLUE}* ${ditem.oldIndex}${TERMC.DEFAULT}`,
+            `<${oldEL.tag}`,
+            `${TERMC.BLUE}* ${ditem.newIndex}${TERMC.DEFAULT}`,
+            `<${newEL.tag}`,
+        ]);
+    } else { util.assertNever(ditem.mostSeriousStatus); }
 
     for (let key of ditem.nochangeKeys) {
         table.push([
@@ -105,18 +98,18 @@ function makeElementChangeTable(ditem: LawDiffElementChange<string>, d: LawDiffR
     for (let [key, status] of ditem.changedKeys) {
         if (status === ProblemStatus.Error) {
             table.push([
-                "", `  ${key}="\x1b[31m${oldEL.attr[key]}\x1b[39m"`,
-                "", `  ${key}="\x1b[31m${newEL.attr[key]}\x1b[39m"`,
+                "", `  ${key}="${TERMC.YELLOW}${oldEL.attr[key]}${TERMC.DEFAULT}"`,
+                "", `  ${key}="${TERMC.YELLOW}${newEL.attr[key]}${TERMC.DEFAULT}"`,
             ]);
         } else if (status === ProblemStatus.Warning) {
             table.push([
-                "", `  ${key}="\x1b[33m${oldEL.attr[key]}\x1b[39m"`,
-                "", `  ${key}="\x1b[33m${newEL.attr[key]}\x1b[39m"`,
+                "", `  ${key}="${TERMC.CYAN}${oldEL.attr[key]}${TERMC.DEFAULT}"`,
+                "", `  ${key}="${TERMC.CYAN}${newEL.attr[key]}${TERMC.DEFAULT}"`,
             ]);
         } else if (status === ProblemStatus.NoProblem) {
             table.push([
-                "", `  ${key}="\x1b[34m${oldEL.attr[key]}\x1b[39m"`,
-                "", `  ${key}="\x1b[34m${newEL.attr[key]}\x1b[39m"`,
+                "", `  ${key}="${TERMC.BLUE}${oldEL.attr[key]}${TERMC.DEFAULT}"`,
+                "", `  ${key}="${TERMC.BLUE}${newEL.attr[key]}${TERMC.DEFAULT}"`,
             ]);
         } else { util.assertNever(status); }
     }
@@ -124,17 +117,17 @@ function makeElementChangeTable(ditem: LawDiffElementChange<string>, d: LawDiffR
     for (let [key, status] of ditem.removedKeys) {
         if (status === ProblemStatus.Error) {
             table.push([
-                "", `  \x1b[31m${key}="${oldEL.attr[key]}"\x1b[39m`,
+                "", `  ${TERMC.YELLOW}${key}="${oldEL.attr[key]}"${TERMC.DEFAULT}`,
                 "", "",
             ]);
         } else if (status === ProblemStatus.Warning) {
             table.push([
-                "", `  \x1b[33m${key}="${oldEL.attr[key]}"\x1b[39m`,
+                "", `  ${TERMC.CYAN}${key}="${oldEL.attr[key]}"${TERMC.DEFAULT}`,
                 "", "",
             ]);
         } else if (status === ProblemStatus.NoProblem) {
             table.push([
-                "", `  \x1b[34m${key}="${oldEL.attr[key]}"\x1b[39m`,
+                "", `  ${TERMC.BLUE}${key}="${oldEL.attr[key]}"${TERMC.DEFAULT}`,
                 "", "",
             ]);
         } else { util.assertNever(status); }
@@ -144,17 +137,17 @@ function makeElementChangeTable(ditem: LawDiffElementChange<string>, d: LawDiffR
         if (status === ProblemStatus.Error) {
             table.push([
                 "", "",
-                "", `  \x1b[31m${key}="${newEL.attr[key]}"\x1b[39m`,
+                "", `  ${TERMC.YELLOW}${key}="${newEL.attr[key]}"${TERMC.DEFAULT}`,
             ]);
         } else if (status === ProblemStatus.Warning) {
             table.push([
                 "", "",
-                "", `  \x1b[33m${key}="${newEL.attr[key]}"\x1b[39m`,
+                "", `  ${TERMC.CYAN}${key}="${newEL.attr[key]}"${TERMC.DEFAULT}`,
             ]);
         } else if (status === ProblemStatus.NoProblem) {
             table.push([
                 "", "",
-                "", `  \x1b[34m${key}="${newEL.attr[key]}"\x1b[39m`,
+                "", `  ${TERMC.BLUE}${key}="${newEL.attr[key]}"${TERMC.DEFAULT}`,
             ]);
         } else { util.assertNever(status); }
     }
@@ -181,7 +174,7 @@ function makeElementNoDiffTable(ditem: LawDiffNoDiff<string>, d: LawDiffResult<s
                     return `<${el.tag} ... />`;
 
                 } else if (tt === TagType.Text) {
-                    return withEllipsis(`${el.text}`, 20);
+                    return withEllipsis(`${el.text}`, LIMIT_WIDTH);
 
                 } else { throw util.assertNever(tt) };
             });
@@ -214,8 +207,8 @@ it("Render and Parse Lawtext", async () => {
     const parsedEL = parse(lawtext);
     analyze(parsedEL);
 
-    const d = lawDiff(origEL.json(false), (parsedEL.json(false)));
-    const table: [string, string, string, string][] = [];
+    const d = lawDiff(origEL.json(false), (parsedEL.json(false)), true);
+    const table: string[][] = [];
 
     for (const ditem of d.items) {
         if (ditem.type === LawDiffType.ElementMismatch) {
@@ -233,7 +226,7 @@ it("Render and Parse Lawtext", async () => {
     if (d.mostSeriousStatus !== ProblemStatus.NoProblem) {
         const msg = [
             `View XML: "${tempOrig}"`,
-            toTableText(table),
+            toTableText(table, LIMIT_WIDTH),
             `View XML: "${tempOrig}"`,
             "",
         ].join("\r\n");
--- js/test/util.ts
+++ js/test/util.ts
@@ -371,7 +371,7 @@ const defaultAttr = new Map([
     ["Style", "solid"],
 ]);
 
-export function lawDiff(oldJson: util.JsonEL, newJson: util.JsonEL) {
+export function lawDiff(oldJson: util.JsonEL, newJson: util.JsonEL, noProblemAsNoDiff: boolean = false) {
 
     const oldEL = new ComparableEL(oldJson);
     const oldELs = [...oldEL.allList()];
@@ -471,7 +471,10 @@ export function lawDiff(oldJson: util.JsonEL, newJson: util.JsonEL) {
                     if (elementMostSeriousStatus < status) elementMostSeriousStatus = status;
                 }
 
-                if (changedKeys.length || removedKeys.length || addedKeys.length) {
+                if (
+                    !(noProblemAsNoDiff && elementMostSeriousStatus === ProblemStatus.NoProblem)
+                    && (changedKeys.length || removedKeys.length || addedKeys.length)
+                ) {
 
                     if (noDiffTable.length) {
                         ret.items.push({
@@ -556,14 +559,77 @@ export function lawDiff(oldJson: util.JsonEL, newJson: util.JsonEL) {
 
 }
 
-function widthWOColor(text: string) {
+export enum TERMC {
+    DEFAULT = "\x1b[39m",
+    RED = "\x1b[31m",
+    GREEN = "\x1b[32m",
+    YELLOW = "\x1b[33m",
+    BLUE = "\x1b[34m",
+    MAGENTA = "\x1b[35m",
+    CYAN = "\x1b[36m",
+}
+
+function sliceWOColor(s: string, start, length) {
+    const maxLen = s.length - start;
+    let ret = "";
+    for (let len = 1; len <= maxLen; len++) {
+        const m = s.slice(start + len - 1).match(/^\x1b\[\d+?m/);
+        if (m) len += m[0].length - 1;
+        const subs = s.slice(start, start + len);
+        if (widthWOColor(subs) <= length) {
+            ret = subs;
+        } else {
+            break;
+        }
+    }
+    return ret;
+}
+
+function* wrapSingle(s: string, width: number): IterableIterator<string> {
+    let pos = 0;
+    let lastFlag = "";
+    while (pos < s.length) {
+        const sliced = sliceWOColor(s, pos, width);
+        pos += sliced.length;
+
+        const flags = sliced.match(/\x1b\[\d+?m/g) || [];
+        let retSubs = `${lastFlag}${sliced}`;
+        if (flags.length) {
+            const flag = flags[flags.length - 1];
+            if (flag === TERMC.DEFAULT) {
+                lastFlag = "";
+            } else {
+                lastFlag = flag;
+                retSubs = `${retSubs}${TERMC.DEFAULT}`;
+            }
+        } else {
+            retSubs = `${retSubs}${TERMC.DEFAULT}`;
+        }
+        yield retSubs;
+    }
+}
+
+function* wrap(row: string[], width: number): IterableIterator<string[]> {
+    let iters = row.map(s => wrapSingle(s, width));
+    while (true) {
+        const nexts = iters.map(iter => iter.next());
+        if (nexts.every(({ done }) => done)) break;
+        yield nexts.map(({ value }) => value || "");
+    }
+}
+
+export function widthWOColor(text: string) {
     return text.replace(/\x1b\[\d+?m/g, "").replace(/[^\x01-\x7E\uFF61-\uFF9F]/g, "  ").length;
 }
 
-export function toTableText(table: string[][]) {
+export function toTableText(table: string[][], width: number) {
     if (!table) return "";
-    const lengths = table[0].map(() => 0);
+    const wrapTable: string[][] = [];
     for (const row of table) {
+        wrapTable.push(...wrap(row, width));
+    }
+    const lengths = wrapTable[0].map(() => 0);
+    for (const row of wrapTable) {
         row.forEach((text, i) => {
             lengths[i] = Math.max(lengths[i], widthWOColor(text));
         });
@@ -571,7 +637,7 @@ export function toTableText(table: string[][]) {
 
     let ret = [
         "┌" + lengths.map(l => "─".repeat(l + 2)).join("┬") + "┐",
-        ...table.map(row =>
+        ...wrapTable.map(row =>
             "│"
             + row.map((text, i) =>
                 " "
@@ -588,8 +654,8 @@ export function toTableText(table: string[][]) {
 }
 
 export function withEllipsis(text: string, maxLength: number) {
-    if (maxLength < text.length) {
-        return `${text.slice(0, maxLength - 2)} ...`;
+    if (maxLength < widthWOColor(text)) {
+        return `${sliceWOColor(text, 0, maxLength - 4)} ...`;
     } else {
         return text;
     }
