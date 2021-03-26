66b91bac122e2b8c7af70f356febcf3c982591cd
yamachi <yamachig@users.noreply.github.com>
2021-03-25 18:39:55 -0700

    LawQueryを修正

--- src/data/query.ts
+++ src/data/query.ts
@@ -1,16 +1,14 @@
 import { LawInfo } from "./lawlist";
 
-abstract class QueryCriteria<TItem, TArgs extends Record<string, unknown> = Record<string, never>> {
-    public constructor(
-        public args: TArgs,
-    ) { }
-    public abstract match(item: TItem): boolean;
+interface QueryCriteria<TItem> {
+    match: (item: TItem) => boolean | Promise<boolean>;
 }
 
-class AllCriteria<TItem> extends QueryCriteria<TItem> {
-    public constructor() { super({}); }
-    public match() { return true; }
-}
+type QueryCriteriaOrFunc<TItem, TQuery extends QueryCriteria<TItem>> = TQuery | QueryCriteria<TItem>["match"];
+
+const AllCriteria = {
+    match: () => true,
+};
 
 interface AsyncIterable<T> {
     [Symbol.asyncIterator](): AsyncIterator<T, void, undefined>;
@@ -18,56 +16,80 @@ interface AsyncIterable<T> {
 
 export class Query<
     TItem,
-    TCriteria = QueryCriteria<TItem, Record<string, never>>,
-    TArgs extends Record<string, unknown> = TCriteria extends QueryCriteria<TItem, infer U> ? U : never,
+    TCriteria extends QueryCriteria<TItem> = QueryCriteria<TItem>,
 > implements AsyncIterable<TItem> {
+
     public constructor (
         public population: AsyncIterable<TItem>,
-        public criteria: QueryCriteria<TItem, TArgs>,
+        public criteria: TCriteria,
     ) {}
+
     async *[Symbol.asyncIterator](): AsyncGenerator<TItem, void, undefined> {
         for await (const item of this.population) {
-            if (!this.criteria.match(item)) continue;
+            const matched = await this.criteria.match(item);
+            if (!matched) continue;
             yield item;
         }
     }
-    public map<T>(func: (item: TItem) => T extends undefined ? never : T): Query<T> {
+
+    public map<T, TRet=T extends void | undefined ? never : T>(func: (item: TItem) => T | Promise<T>): Query<TRet> {
         // eslint-disable-next-line @typescript-eslint/no-this-alias
         const self = this;
         return new Query(
             (async function *() {
                 for await (const item of self) {
-                    const value = func(item);
+                    const value = await func(item);
                     if (value === undefined) {
                         throw TypeError(`Query.map: the mapped function (${func}) returned an undefined. Please check the definition of the function. (First occurance: ${item})`);
                     }
-                    yield value;
+                    yield value as unknown as TRet;
                 }
             })(),
-            new AllCriteria<TItem>(),
+            AllCriteria,
         );
     }
+
+    public filter<
+        TQueryCriteriaOrFunc extends QueryCriteriaOrFunc<TItem, QueryCriteria<TItem>>,
+        TRetCriteria extends QueryCriteria<TItem>
+            = TQueryCriteriaOrFunc extends QueryCriteriaOrFunc<TItem, infer TNewCriteria>
+                ? TNewCriteria
+                : QueryCriteria<TItem>,
+    >(criteriaOrFunc: TQueryCriteriaOrFunc): Query<TItem, TRetCriteria> {
+        return new Query<TItem, TRetCriteria>(
+            this[Symbol.asyncIterator](),
+            ("match" in criteriaOrFunc
+                ? criteriaOrFunc
+                : { match: criteriaOrFunc }) as unknown as TRetCriteria,
+        );
+    }
+
     public async toArray(): Promise<TItem[]> {
         const arr: TItem[] = [];
+        await this.forEach(item => arr.push(item));
+        return arr;
+    }
+
+    public async forEach(func: (item: TItem) => unknown | Promise<unknown>): Promise<void> {
         const startTime = new Date();
         let lastMessageTime = startTime;
         let matchCount = 0;
         for await (const item of this) {
             matchCount++;
-            arr.push(item);
+            await func(item);
             const now = new Date();
             if (now.getTime() - lastMessageTime.getTime() > 1000) {
-                console.info(`Query.toArray:\t⌛ running...\t(${matchCount} matches\tin ${now.getTime() - startTime.getTime()} msec)`);
+                console.info(`Query progress:\t⌛ running...\t(${matchCount} matches\tin ${now.getTime() - startTime.getTime()} msec)`);
                 lastMessageTime = now;
             }
         }
         const now = new Date();
         const msec = now.getTime() - startTime.getTime();
         if (msec > 1000) {
-            console.info(`Query.toArray:\t✓ completed.\t(${matchCount} matches\tin ${msec} msec)`);
+            console.info(`Query progress:\t✓ completed.\t(${matchCount} matches\tin ${msec} msec)`);
         }
-        return arr;
     }
+
 }
 
 const getDefaultLawCriteriaArgs = () => ({
@@ -80,7 +102,10 @@ const getDefaultLawCriteriaArgs = () => ({
     ReferencedLawNum: undefined as RegExp | undefined,
 });
 export type LawCriteriaArgs = ReturnType<typeof getDefaultLawCriteriaArgs>;
-export class LawCriteria extends QueryCriteria<LawQueryItem, LawCriteriaArgs> {
+export class LawCriteria implements QueryCriteria<LawQueryItem> {
+
+    public args: LawCriteriaArgs;
+
     public constructor(args: Partial<LawCriteriaArgs>){
         const defaults = getDefaultLawCriteriaArgs();
         const invalidKeys: [key: string, message: string][] = [];
@@ -98,8 +123,9 @@ export class LawCriteria extends QueryCriteria<LawQueryItem, LawCriteriaArgs> {
         if (invalidKeys.length > 0) {
             throw new TypeError(`LawCriteria: invalid key(s): ${invalidKeys.map(([key, message]) => `"${key}" (${message})`).join(", ")}`);
         }
-        super({ ...defaults, ...args });
+        this.args = { ...defaults, ...args };
     }
+
     match(item: LawQueryItem): boolean {
         if (this.args.LawID !== undefined && !this.args.LawID.exec(item.LawID)) return false;
         if (this.args.LawNum !== undefined && !this.args.LawNum.exec(item.LawNum)) return false;
@@ -150,6 +176,7 @@ export class LawQueryItem extends LawInfo {
 
 export class LawQuery
     extends Query<LawQueryItem, LawCriteria> {
+
     public constructor (
         population: AsyncIterable<LawQueryItem>,
         criteriaOrArgs: LawCriteria | LawCriteriaArgs,
@@ -161,6 +188,7 @@ export class LawQuery
                 : new LawCriteria(criteriaOrArgs),
         );
     }
+
     public static fromPromiseLawInfos(
         pLawList: Promise<LawInfo[]>,
         criteriaOrArgs: LawCriteria | LawCriteriaArgs,
