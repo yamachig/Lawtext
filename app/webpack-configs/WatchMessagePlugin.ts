836962a72612a252fbbd21f0f68d6f961f6401df
yamachi <yamachig@users.noreply.github.com>
2021-03-26 08:30:39 -0700

    WatchMessagePluginを修正

--- app/webpack-configs/WatchMessagePlugin.ts
+++ app/webpack-configs/WatchMessagePlugin.ts
@@ -2,6 +2,9 @@ import webpack from "webpack";
 
 export default class WatchMessagePlugin {
     public apply(compiler: webpack.Compiler): void {
+        compiler.hooks.beforeRun.tap("WatchMessagePlugin", () => {
+            console.log("\x1b[36m" + "Begin compile at " + new Date() + " \x1b[39m");
+        });
         compiler.hooks.watchRun.tap("WatchMessagePlugin", () => {
             console.log("\x1b[36m" + "Begin compile at " + new Date() + " \x1b[39m");
         });
