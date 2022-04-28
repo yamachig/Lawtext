1db6cde9739b5f72eaca14b5ac66397a438b57ca
yamachi <yamachig@users.noreply.github.com>
2022-04-27 22:41:56 -0700

    ContainerRefを修正

--- app/src/components/LawView/ControlRun.tsx
+++ app/src/components/LawView/ControlRun.tsx
@@ -1,5 +1,5 @@
 
-import React from "react";
+import React, { Fragment } from "react";
 import * as std from "lawtext/dist/src/law/std";
 import { HTMLComponentProps, HTMLMarginSpan, WrapperComponentProps } from "lawtext/dist/src/renderer/common/html";
 import { HTMLSentenceChildrenRun } from "lawtext/dist/src/renderer/rules/sentenceChildrenRun";
@@ -30,15 +30,15 @@ export const WrapHTMLControlRun: React.FC<WrapperComponentProps> = props => {
         const declaration = analysis.declarations.get(el.attr.declarationID);
         const declContainer = analysis.sentenceEnvs[declaration.nameSentenceTextRange.start.sentenceIndex].container;
         const containerID = declContainer.containerID;
-        return <ContainerRef containerID={containerID} sentenceChildren={sentenceChildren} {...{ htmlOptions }} />;
+        return <ContainerRef containerIDs={[containerID]} sentenceChildren={sentenceChildren} {...{ htmlOptions }} />;
 
     } else if (el instanceof ____PF) {
         const options = htmlOptions.options as LawViewOptions;
         const analysis = options.lawData.analysis;
         const sentenceChildren = el.children as (string | SentenceChildEL)[];
         if (!analysis || el.targetContainerIDs.length === 0) return (<HTMLSentenceChildrenRun els={sentenceChildren} {...{ htmlOptions }} />);
-        const containerID = el.targetContainerIDs.slice(-1)[0];
-        return <ContainerRef containerID={containerID} sentenceChildren={sentenceChildren} {...{ htmlOptions }} />;
+        const containerIDs = el.targetContainerIDs;
+        return <ContainerRef containerIDs={containerIDs as string[]} sentenceChildren={sentenceChildren} {...{ htmlOptions }} />;
 
     } else if (el instanceof ____LawNum) {
         return <LawNum el={el} {...{ htmlOptions }} />;
@@ -124,12 +124,12 @@ const ContainerRefWindowSpan = styled.span`
     background-color: rgba(240, 240, 240);
 `;
 
-interface ContainerRefProps { containerID: string, sentenceChildren: (string | SentenceChildEL)[] }
+interface ContainerRefProps { containerIDs: string[], sentenceChildren: (string | SentenceChildEL)[] }
 
 interface ContainerRefState { mode: ContainerRefFloatState, arrowLeft: string }
 
 const ContainerRef = (props: HTMLComponentProps & ContainerRefProps) => {
-    const { containerID, sentenceChildren, htmlOptions } = props;
+    const { containerIDs, sentenceChildren, htmlOptions } = props;
 
 
     const refText = React.useRef<HTMLSpanElement>(null);
@@ -220,7 +220,7 @@ const ContainerRef = (props: HTMLComponentProps & ContainerRefProps) => {
                         <ContainerRefFloatBlockInnerSpan>
                             <ContainerRefArrowSpan style={state.arrowLeft ? { marginLeft: state.arrowLeft } : { visibility: "hidden" }} />
                             <ContainerRefWindowSpan ref={refWindow}>
-                                <PeekContainerView containerID={containerID} {...{ htmlOptions }} />
+                                <PeekContainerView containerIDs={containerIDs} {...{ htmlOptions }} />
                             </ContainerRefWindowSpan>
                         </ContainerRefFloatBlockInnerSpan>
                     )}
@@ -232,137 +232,143 @@ const ContainerRef = (props: HTMLComponentProps & ContainerRefProps) => {
 };
 
 
-interface PeekContainerViewProps { containerID: string }
+interface PeekContainerViewProps { containerIDs: string[] }
 
 const PeekContainerView = (props: HTMLComponentProps & PeekContainerViewProps) => {
-    const { containerID, htmlOptions } = props;
+    const { containerIDs, htmlOptions } = props;
     const options = htmlOptions.options as LawViewOptions;
 
     const analysis = options.lawData.analysis;
     if (!analysis) return null;
 
-    const container = analysis.containers.get(containerID);
-    if (!container) return null;
-    const containerStack = container.linealAscendant(c => {
-        if (std.isParagraph(c.el)) {
-            const paragraphNum = c.el.children.find(std.isParagraphNum);
-            if (!c.parent) return true;
-            if (
-                std.isArticle(c.parent.el) &&
+    const ret: JSX.Element[] = [];
+
+    for (const containerID of containerIDs) {
+
+        const container = analysis.containers.get(containerID);
+        if (!container) return null;
+        const containerStack = container.linealAscendant(c => {
+            if (std.isParagraph(c.el)) {
+                const paragraphNum = c.el.children.find(std.isParagraphNum);
+                if (!c.parent) return true;
+                if (
+                    std.isArticle(c.parent.el) &&
                 c.parent.children.filter(pc => std.isParagraph(pc.el)).length === 1 &&
                 paragraphNum && paragraphNum.text() === ""
-            ) {
-                return false;
+                ) {
+                    return false;
+                } else {
+                    return true;
+                }
             } else {
                 return true;
             }
-        } else {
-            return true;
-        }
-    });
-    const names: string[] = [];
+        });
+        const names: string[] = [];
 
-    const titleTags = [
-        "LawTitle",
-        "ArticleTitle",
-        ...std.paragraphItemTitleTags,
-        ...std.articleGroupTitleTags,
-        ...std.appdxItemTitleTags,
-        ...std.supplProvisionAppdxItemTitleTags,
-        "SupplProvisionLabel",
-        "TableStructTitle",
-    ];
-    const ignoreTags = ["ArticleCaption", "ParagraphCaption", ...titleTags];
+        const titleTags = [
+            "LawTitle",
+            "ArticleTitle",
+            ...std.paragraphItemTitleTags,
+            ...std.articleGroupTitleTags,
+            ...std.appdxItemTitleTags,
+            ...std.supplProvisionAppdxItemTitleTags,
+            "SupplProvisionLabel",
+            "TableStructTitle",
+        ];
+        const ignoreTags = ["ArticleCaption", "ParagraphCaption", ...titleTags];
 
-    for (const c of containerStack) {
-        if ((std.isLaw(container.el) || std.isMainProvision(container.el)) && std.isLaw(c.el)) {
-            const lawTitle = c.el.children.find(std.isLawBody)?.children.find(std.isLawTitle);
-            const lawNum = c.el.children.find(std.isLawNum);
-            if (lawTitle && lawNum) {
-                names.push(`${lawTitle.text()}（${lawNum.text()}）`);
-            } else if (lawTitle) {
-                names.push(lawTitle.text());
-            } else if (lawNum) {
-                names.push(lawNum.text());
+        for (const c of containerStack) {
+            if ((std.isLaw(container.el) || std.isMainProvision(container.el)) && std.isLaw(c.el)) {
+                const lawTitle = c.el.children.find(std.isLawBody)?.children.find(std.isLawTitle);
+                const lawNum = c.el.children.find(std.isLawNum);
+                if (lawTitle && lawNum) {
+                    names.push(`${lawTitle.text()}（${lawNum.text()}）`);
+                } else if (lawTitle) {
+                    names.push(lawTitle.text());
+                } else if (lawNum) {
+                    names.push(lawNum.text());
+                }
+
+            } else if (std.isEnactStatement(c.el)) {
+                names.push("（制定文）");
+
+            } else if (std.isArticleGroup(container.el) && std.isArticleGroup(c.el)) {
+                const articleGroupTitle = (c.el.children as (typeof c.el.children)[number][])
+                    .find(std.isArticleGroupTitle);
+                if (articleGroupTitle) names.push(articleGroupTitle.text());
+
+            } else if (std.isSupplProvision(c.el)) {
+                const supplProvisionLabel = c.el.children
+                    .find(std.isSupplProvisionLabel);
+                if (supplProvisionLabel) names.push(supplProvisionLabel.text());
+
+            } else if (std.isArticle(c.el)) {
+                const articleTitle = c.el.children
+                    .find(std.isArticleTitle);
+                if (articleTitle) names.push(articleTitle.text());
+
+            } else if (std.isParagraph(c.el)) {
+                const paragraphNum = c.el.children
+                    .find(std.isParagraphNum);
+                if (paragraphNum) names.push(paragraphNum.text() || "１");
+
+            } else if (std.isParagraphItem(c.el)) {
+                const itemTitle = (c.el.children as EL[])
+                    .find(std.isParagraphItemTitle);
+                if (itemTitle) names.push(itemTitle.text());
+
+            } else if (std.isTableStruct(c.el)) {
+                const tableStructTitleEl = c.el.children
+                    .find(std.isTableStructTitle);
+                const tableStructTitle = tableStructTitleEl
+                    ? tableStructTitleEl.text()
+                    : "表";
+                names.push(tableStructTitle + "（抜粋）");
+
+            } else {
+                continue;
             }
+        }
 
-        } else if (std.isEnactStatement(c.el)) {
-            names.push("（制定文）");
+        const containerElTitleTag = std.isMainProvision(container.el) ? "LawTitle" : titleTags
+            .find(s => s.startsWith(container.el.tag));
 
-        } else if (std.isArticleGroup(container.el) && std.isArticleGroup(c.el)) {
-            const articleGroupTitle = (c.el.children as (typeof c.el.children)[number][])
-                .find(std.isArticleGroupTitle);
-            if (articleGroupTitle) names.push(articleGroupTitle.text());
+        if (containerElTitleTag) {
+            const containerEl = new EL(
+                std.isMainProvision(container.el) ? "Law" : container.el.tag,
+                {},
+                [
+                    ...(
+                        (std.isLaw(container.el) || std.isMainProvision(container.el))
+                            ? [new EL("LawBody", {}, [new EL("LawTitle", {}, [names.join("／")])])]
+                            : [new EL(containerElTitleTag, {}, [names.join("／")])]
+                    ),
+                    ...(
+                        (std.isLaw(container.el) || std.isMainProvision(container.el) || std.isArticleGroup(container.el) || std.isSupplProvision(container.el))
+                            ? []
+                            : (container.el.children as EL[])
+                                .filter(child => ignoreTags.indexOf(child.tag) < 0)
+                    ),
+                ],
+            );
+            ret.push(<HTMLAnyELs els={[containerEl as std.StdEL]} indent={0} {...{ htmlOptions }} />);
 
-        } else if (std.isSupplProvision(c.el)) {
-            const supplProvisionLabel = c.el.children
-                .find(std.isSupplProvisionLabel);
-            if (supplProvisionLabel) names.push(supplProvisionLabel.text());
-
-        } else if (std.isArticle(c.el)) {
-            const articleTitle = c.el.children
-                .find(std.isArticleTitle);
-            if (articleTitle) names.push(articleTitle.text());
-
-        } else if (std.isParagraph(c.el)) {
-            const paragraphNum = c.el.children
-                .find(std.isParagraphNum);
-            if (paragraphNum) names.push(paragraphNum.text() || "１");
-
-        } else if (std.isParagraphItem(c.el)) {
-            const itemTitle = (c.el.children as EL[])
-                .find(std.isParagraphItemTitle);
-            if (itemTitle) names.push(itemTitle.text());
-
-        } else if (std.isTableStruct(c.el)) {
-            const tableStructTitleEl = c.el.children
-                .find(std.isTableStructTitle);
-            const tableStructTitle = tableStructTitleEl
-                ? tableStructTitleEl.text()
-                : "表";
-            names.push(tableStructTitle + "（抜粋）");
+        } else if (std.isEnactStatement(container.el)) {
+            ret.push(
+                <div style={{ paddingLeft: "1em", textIndent: "-1em" }}>
+                    <span>{names.join("／")}</span>
+                    <HTMLMarginSpan/>
+                    <HTMLSentenceChildrenRun els={container.el.children} {...{ htmlOptions }} />
+                </div>,
+            );
 
         } else {
-            continue;
+            throw new NotImplementedError(container.el.tag);
+
         }
     }
-
-    const containerElTitleTag = std.isMainProvision(container.el) ? "LawTitle" : titleTags
-        .find(s => s.startsWith(container.el.tag));
-
-    if (containerElTitleTag) {
-        const containerEl = new EL(
-            std.isMainProvision(container.el) ? "Law" : container.el.tag,
-            {},
-            [
-                ...(
-                    (std.isLaw(container.el) || std.isMainProvision(container.el))
-                        ? [new EL("LawBody", {}, [new EL("LawTitle", {}, [names.join("／")])])]
-                        : [new EL(containerElTitleTag, {}, [names.join("／")])]
-                ),
-                ...(
-                    (std.isLaw(container.el) || std.isMainProvision(container.el) || std.isArticleGroup(container.el) || std.isSupplProvision(container.el))
-                        ? []
-                        : (container.el.children as EL[])
-                            .filter(child => ignoreTags.indexOf(child.tag) < 0)
-                ),
-            ],
-        );
-        return <HTMLAnyELs els={[containerEl as std.StdEL]} indent={0} {...{ htmlOptions }} />;
-
-    } else if (std.isEnactStatement(container.el)) {
-        return (
-            <div style={{ paddingLeft: "1em", textIndent: "-1em" }}>
-                <span>{names.join("／")}</span>
-                <HTMLMarginSpan/>
-                <HTMLSentenceChildrenRun els={container.el.children} {...{ htmlOptions }} />
-            </div>
-        );
-
-    } else {
-        throw new NotImplementedError(container.el.tag);
-
-    }
+    return <>{ret.map((el, i) => <Fragment key={i}>{el}</Fragment>)}</>;
 };
 
 
