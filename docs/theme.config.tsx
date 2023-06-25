17ba8a2097d69dfaf83f1d072f6ef7b89ddbd6ab
yamachi <yamachig@users.noreply.github.com>
2023-06-25 15:13:34 +0900

    docsを修正

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -6,10 +6,15 @@ const config: DocsThemeConfig = {
     head: () => null,
     editLink: { component: () => null },
     feedback: { content: () => null },
-    footer: { text: () => null },
+    footer: { text: () => (<div><small>
+        &copy; 2017-{new Date().getFullYear()} <a href="https://github.com/yamachig/" target="_blank" rel="noreferrer">yamachi</a>
+    </small></div>) },
     faviconGlyph: "L",
     gitTimestamp: () => null,
     useNextSeoProps: () => ({ titleTemplate: "%s - Lawtext" }),
+    nextThemes: {
+        defaultTheme: "light",
+    },
 };
 
 export default config;
