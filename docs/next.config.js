6253d19fdec56eed167a0babd740cac04d76c9c7
yamachi <yamachig@users.noreply.github.com>
2023-06-25 16:48:10 +0900

    next.config.jsを修正

--- docs/next.config.js
+++ docs/next.config.js
@@ -4,15 +4,17 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const nextra = require("nextra");
 
+
 const withNextra = nextra({
     theme: "nextra-theme-docs",
     themeConfig: "./theme.config.tsx",
 });
 
-module.exports = withNextra({
+const config = {
     images: {
         unoptimized: true,
     },
     trailingSlash: true,
-});
+};
+module.exports = withNextra(config);
 
